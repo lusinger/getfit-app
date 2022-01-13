75f13db87058a5c823a01675854665682b0921ff
Lukas Singer <lukas.singer@outlook.com>
2022-01-13 11:48:36 +0100

    Implement checkWhitespace validator in LoginComponent
    -Need to fix validator does not work yet

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,9 +1,12 @@
 //TODO: Create and add service containing login method to reach server
 //TODO: Redirect to UserPanel if login is successfull
-//TODO: Create Validator that disallows whitespaces
+//FIXME: validator does not return error if whitespace is added
 //TODO: Create and add Directive that typewrites title of form
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+//--import for whitespace validator to refuse usernames white leading and trailing whitespaces
+import { checkWhitespace } from 'src/app/validators/check-whitespace';
 @Component({
   selector: 'getfit-login',
   templateUrl: './login.component.html',
@@ -11,7 +14,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   loginForm = new FormGroup({
-    user: new FormControl('', [Validators.required, ]),
+    user: new FormControl('', [checkWhitespace(), Validators.required, ]),
     password: new FormControl('', [Validators.required, ]),
   });
 
