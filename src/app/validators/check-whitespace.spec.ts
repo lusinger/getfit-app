7c83f8b1ad3df2b28cb8280d73d5680a78453e41
Lukas Singer <lukas.singer@outlook.com>
2022-01-13 12:11:22 +0100

    Fix checkWhitespace validator and spec file
    -Use regular expression to check for whitespaces

--- src/app/validators/check-whitespace.spec.ts
+++ src/app/validators/check-whitespace.spec.ts
@@ -6,22 +6,17 @@ describe('checkWhitespace()', () => {
   const formField = new FormControl('');
 
   it('should return null if no whitespace is detected at the beginning and end of field value', () => {
-    formField.setValue('test value');
+    formField.setValue('testvalue');
     expect(validator(formField)).toBeNull();
   });
 
-  it('should return error "WHITESPACE_NOT_ALLOWED" if whitespaces is detected at the beggining', () => {
-    formField.setValue(' testValue');
-    expect(validator(formField)).toEqual(new Error('WHITESPACE_NOT_ALLOWED'));
-  });
-
-  it('should return error "WHITESPACE_NOT_ALLOWED" if whitespace is detected at the end', () => {
-    formField.setValue('testValue ');
-    expect(validator(formField)).toEqual(new Error('WHITESPACE_NOT_ALLOWED'));
+  it('should return error "WHITESPACE_NOT_ALLOWED" if whitespaces is detected', () => {
+    formField.setValue('test Value');
+    expect(validator(formField)).toEqual({error: new Error('WHITESPACE_NOT_ALLOWED')});
   });
 
   it('should return error "EMPTY_STRING_NOT_ALLOWED" if string is ""', () => {
     formField.reset();
-    expect(validator(formField)).toEqual(new Error('EMPTY_STRING_NOT_ALLOWED'));
+    expect(validator(formField)).toEqual({error: new Error('EMPTY_STRING_NOT_ALLOWED')});
   })
 });
