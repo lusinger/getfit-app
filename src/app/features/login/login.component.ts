59fb6e798ed8354602c6343a4fd16ba58fb6c43e
Lukas Singer <lukas.singer@outlook.com>
2022-01-13 12:37:36 +0100

    Remove FIXME comment

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,6 +1,5 @@
 //TODO: Create and add service containing login method to reach server
 //TODO: Redirect to UserPanel if login is successfull
-//FIXME: validator does not return error if whitespace is added
 //TODO: Create and add Directive that typewrites title of form
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
