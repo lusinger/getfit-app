1bb1ea4bf657fd817a0e3498e4913abd9787e651
Lukas Singer <lukas.singer@outlook.com>
2022-01-13 16:30:05 +0100

    Add login method to AuthService
    -Add spec file test for login method
    -Add serverUrl environment variable

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,48 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable, throwError, catchError, retry } from 'rxjs';
 
 import { LoginRequest } from '../interfaces/login-request';
 import { AuthResponse } from '../interfaces/auth-response';
 
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  defaultHeader = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
 
-  constructor() { }
+  constructor(private http: HttpClient,) { }
+
+  private handleLoginErrors(error: HttpErrorResponse){
+    switch(error.status){
+      case 0:
+        console.log('client or network error');
+        break;
+      case 200:
+        console.log('successful login');
+        break;
+      case 401:
+        console.log('login not authorized');
+        break;
+    }
+    return throwError(() => new Error('error occured'));
+  }
+
+  login(loginData: LoginRequest): Observable<AuthResponse>{
+    const params = new HttpParams()
+      .set('user', loginData.user)
+      .set('password', loginData.password);
+    
+    return this.http.get<AuthResponse>(`${environment.serverUrl}/login`, {
+      headers: this.defaultHeader, 
+      params: params,
+      observe: 'body',
+      responseType: 'json',
+    }).pipe(catchError(this.handleLoginErrors));
+  }
 }
+ 
