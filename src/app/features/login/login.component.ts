f7925a750d115045596283d4c99be6691b3495ee
Lukas Singer <lukas.singer@outlook.com>
2022-01-13 10:46:41 +0100

    Add TODO's at top of LoginComponent

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,3 +1,9 @@
+//TODO: Create FromGroup and Control for loginForm
+//TODO: Disable submit button while loginForm is not valid
+//TODO: Create and add service containing login method to reach server
+//TODO: Redirect to UserPanel if login is successfull
+//TODO: Create Validator that disallows whitespaces
+//TODO: Create and add Directive that typewrites title of form
 import { Component, OnInit } from '@angular/core';
 
 @Component({
