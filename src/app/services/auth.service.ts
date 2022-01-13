614005e535cbe76a015277b49e35f1f83d19f65d
Lukas Singer <lukas.singer@outlook.com>
2022-01-13 14:55:30 +0100

    Add LoginRequest and AuthResponse interface for server communication
    -Add HttpClientModule to AppModule
    -Add HttpClient and rxjs classes to AuthService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError, catchError, retry } from 'rxjs';
+
+import { LoginRequest } from '../interfaces/login-request';
+import { AuthResponse } from '../interfaces/auth-response';
 
 @Injectable({
   providedIn: 'root'
