745e44d368ffd0b753cfe996728d58ba183cba5d
Lukas Singer <lukas.singer@outlook.com>
2022-01-14 11:14:25 +0100

    Add Formroup registerForm with needed FormControls
    -Add ReactiveFormsModule to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TypewriterDirective } from './directives/typewriter.directive';
@@ -15,6 +16,7 @@ import { RegisterComponent } from './features/register/register.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'getfit-register',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.sass']
 })
 export class RegisterComponent implements OnInit {
+  registerForm = new FormGroup({
+    userName: new FormControl('', [Validators.required, ]),
+    mail: new FormControl('', [Validators.required, ]),
+    password: new FormControl('', [Validators.required, ]),
+    retype: new FormControl('', [Validators.required, ]),
+    fullName: new FormControl('', [Validators.required, ]),
+    age: new FormControl('', [Validators.required, ]),
+    height: new FormControl('', [Validators.required, ]),
+    currentWeight: new FormControl('', [Validators.required, ]),
+    targetWeight: new FormControl('', [Validators.required, ]),
+    changePerWeek: new FormControl('', [Validators.required, ]),
+    gender: new FormControl('', [Validators.required, ]),
+  });
+
+  formTitle: string = 'getfit';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onTextChanged($event: string): void{
+    this.formTitle = $event;
+  }
+
+  onRegisterSubmit(): void{
+    
+  }
 }
