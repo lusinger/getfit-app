3e7d939c36e9c6c7b78d580e7b6dae6bb2680384
Lukas Singer <lukas.singer@outlook.com>
2022-01-17 07:41:02 +0100

    Add TrackingSectionComponent to project and added instances to UserPanel

---
+++ src/app/features/user-panel/tracking-section/tracking-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'getfit-tracking-section',
+  templateUrl: './tracking-section.component.html',
+  styleUrls: ['./tracking-section.component.sass']
+})
+export class TrackingSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
