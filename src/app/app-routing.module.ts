97581b03838afa615388ab6aec34cc6956028248
Lukas Singer <lukas.singer@outlook.com>
2022-01-18 10:12:52 +0100

    Remove remaining merge indiactors to fix build error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,15 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import { RegisterComponent } from './features/register/register.component';
-import { ResetComponent } from './features/reset/reset.component';
-
-const routes: Routes = [
-  {path: 'reset', component: ResetComponent},
-  {path: 'register', component: RegisterComponent},
-=======
 import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
+import { ResetComponent } from './features/reset/reset.component';
 import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'reset', component: ResetComponent},
   {path: 'register', component: RegisterComponent},
   {path: '**', redirectTo: 'login'},
->>>>>>> development
 ]
 
 
