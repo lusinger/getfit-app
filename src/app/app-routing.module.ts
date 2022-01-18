307d650d0cc9da71a5c3693a10235c9d512ac898
Lukas Singer <36990027+lusinger@users.noreply.github.com>
2022-01-18 15:51:08 +0100

    Feature reset (#21)
    * Create ResetComponent to reset user mail
    
    -Add markup and styling to all needed components
    -Impement Typewriter Directive
    
    * Add form to send mail and form to send new password
    
    -The two different forms get displayed depending on the form state
    
    * Add reset and register route to RouterModule
    
    -Add method to navigate to different pages in ResetComponent
    
    * Activate reset form
    
    * Implement AuthService in ResetComponent
    
    -Add resetPassword method to onSubmit
    
    * Remove remaining merge indiactors to fix build error
    
    * Add mail and password reset form submits
    
    * Add router outlet to allow routing from reset to login and register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
+import { ResetComponent } from './features/reset/reset.component';
 import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'reset', component: ResetComponent},
   {path: 'register', component: RegisterComponent},
   {path: '**', redirectTo: 'login'},
 ]
