df51d0b6baecbad43a59478a79290707f28a2596
Lukas Singer <lukas.singer@outlook.com>
2022-01-18 09:35:15 +0100

    Create markup and styling of SearchOverlayComponent

--- src/app/features/user-panel/search-overlay/search-overlay.component.ts
+++ src/app/features/user-panel/search-overlay/search-overlay.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'getfit-search-overlay',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-overlay.component.sass']
 })
 export class SearchOverlayComponent implements OnInit {
+  searchForm = new FormGroup({
+    search: new FormControl('', [Validators.required, ]),
+  });
+
+  searchValue: string | ' ' = ' ';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onInputChange($event: any): void{
+    this.searchValue = $event.target.value;
+  }
 }
