855131dfece73265fc957eb4535cac162ad67ac1
Lukas Singer <lukas.singer@outlook.com>
2022-01-19 15:34:36 +0100

    Create styling for calendar and calendar fields
    -Transition from active inactive and default
    -Add scroll lock while DatePicker overlay is open

--- src/app/features/user-panel/date-picker/date-picker.component.ts
+++ src/app/features/user-panel/date-picker/date-picker.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatePickerComponent implements OnInit {
 
+  days: string[] = ['Mo', 'Th', 'Tu', 'We', 'Fr', 'Sa', 'Su'];
+  fields: number[] = [1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 1, 31, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7,]
+
   constructor() { }
 
   ngOnInit(): void {
+    addEventListener('scroll', () => {
+      window.scrollTo(0, 0);
+    });
   }
-
 }
