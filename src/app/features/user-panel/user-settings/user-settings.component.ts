97ae3e1738b20b66856f21c82acb5834a6213b0f
Lukas Singer <36990027+lusinger@users.noreply.github.com>
2022-01-20 11:44:02 +0100

    Subfeature user settings (#31)
    * Clean up content for UserSettings to redo the settup
    
    * Style and markup of settings page
    
    * Create toggle function and animation for the user settings
    
    -Disallow scroll while settings page is open

--- src/app/features/user-panel/user-settings/user-settings.component.ts
+++ src/app/features/user-panel/user-settings/user-settings.component.ts
@@ -1,54 +1,41 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { User } from 'src/app/interfaces/user';
-
-import { checkPassword } from 'src/app/validators/check-password';
+import { Component, OnInit} from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'getfit-user-settings',
   templateUrl: './user-settings.component.html',
   styleUrls: ['./user-settings.component.sass'],
   animations: [
-    trigger('inOut', [
+    trigger('toggleSettings', [
       transition(':enter', [
-        style({opacity: 1, transform: 'translateY(-100%)'}),
-        animate(500, style({opacity: 1, transform: 'translateY(0%)'})),
+        style({transform: 'translateY(-100%)'}),
+        animate(350, style({transform: 'translateY(0)'})),
       ]),
       transition(':leave', [
-        style({opacity: 1, transform: 'translateY(0%)'}),
-        animate(500, style({opacity: 1, transform: 'translateY(-100%)'})),
+        style({transform: 'translateY(0)'}),
+        animate(350, style({transform: 'translateY(-100%)'})),
       ]),
-    ])
+    ]),
   ]
 })
 export class UserSettingsComponent implements OnInit {
-  @Input() userData: User | null = null;
-  @Input() settingsState: 'open' | 'closed' = 'closed';
-
-  @Output() onSettingClose = new EventEmitter<'open' | 'closed'>();
-
-  settingsForm = new FormGroup({
-    userName: new FormControl('', []),
-    mail: new FormControl('', [Validators.email]),
-    oldPassword: new FormControl('', [Validators.minLength(8)]),
-    password: new FormControl('', [Validators.minLength(8)]),
-    retype: new FormControl('', [Validators.minLength(8)]),
-    fullName: new FormControl('', []),
-    age: new FormControl('', []),
-    height: new FormControl('', []),
-    currentWeight: new FormControl('', []),
-    targetWeight: new FormControl('', []),
-    changePerWeek: new FormControl('', []),
-    gender: new FormControl('', []),
-  }, [checkPassword('password', 'retype')]);
+  settingsState: 'open' | 'closed' = 'closed';
 
   constructor() { }
 
   ngOnInit(): void {
+    addEventListener('scroll', () => {
+      if(this.settingsState === 'open'){
+        window.scrollTo(0, 0);
+      }
+    });
   }
 
-  closeSettings(): void{
-    this.onSettingClose.emit('closed');
+  toggleSettings(): void{
+    if(this.settingsState === 'open'){
+      this.settingsState = 'closed';
+    }else{
+      this.settingsState = 'open';
+    }
   }
 }
