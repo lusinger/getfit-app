0f71b4c138e08c1c115ecc95d29b1148d553c108
Lukas Singer <lukas.singer@outlook.com>
2022-01-29 10:45:18 +0100

    Add selectedDate state to stateMachine

--- src/app/services/state-machine.service.ts
+++ src/app/services/state-machine.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateMachineService {
+  _selectedDate = new BehaviorSubject<Date>(new Date());
+  selectedDate = this._selectedDate.asObservable();
 
   constructor() { }
+
+  getSelectedDate(): Observable<Date>{
+    return this.selectedDate;
+  }
+  setSelectedDate(date: Date){
+    this._selectedDate.next(date);
+  }
 }
